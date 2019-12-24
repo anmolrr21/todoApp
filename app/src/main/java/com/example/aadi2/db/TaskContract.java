a479d125f0443c7baa7231b252eb98afb34251da
anmolrr21 <varsharohra3april@gmail.com>
2019-12-24 16:12:18 +0530

    init

---
+++ app/src/main/java/com/example/aadi2/db/TaskContract.java
@@ -0,0 +1,23 @@
+package com.example.aadi2.db;
+
+import android.provider.BaseColumns;
+
+
+
+
+    public class TaskContract {
+        public static final String DB_NAME = "com.example.aadi2";
+        public static final int DB_VERSION = 1;
+
+        public class TaskEntry implements BaseColumns {
+            public static final String TABLE = "tasks";
+
+            public static final String COL_TASK_TITLE = "title";
+        }
+    }
+
+
+
+
+
+
